da713987d1b09ab94cab99ffc218f6323cc54205
Hamlet Poghosian <phamik@mail.ru>
2019-04-10 11:59:16 +0400

    Add new Class

--- UsersData/UsersData/Models/Users.cs
+++ UsersData/UsersData/Models/Users.cs
@@ -7,5 +7,10 @@ namespace UsersData.Models
 {
 	public class Users
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Lastname { get; set; }
+		public int Solary { get; set; }
+
 	}
 }
